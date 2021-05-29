ceafd72e7a4daddf9bdc4c95e3a96fd778aa784c
rti56kt <rti56kt@gmail.com>
2021-05-30 01:00:26 +0800

    [QRSCAN-5] Add A2HS button; Add info button

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-if ('serviceWorker' in navigator) {
+if('serviceWorker' in navigator) {
     window.addEventListener('DOMContentLoaded', function() {
         navigator.serviceWorker.register('/a2hs-qrscanner/sw.js')
         .then(reg => console.log('😎 Service worker registered!', reg))
@@ -7,7 +7,9 @@ if ('serviceWorker' in navigator) {
 }
 
 window.addEventListener('DOMContentLoaded', function() {
+    var installPromptEvent;
     var scanner = new Instascan.Scanner({ video: document.getElementById('preview'), mirror: false });
+
     scanner.addListener('scan', function (content) {
         $('#exampleModal').modal('show');
         $('#ModalContent').text(content);
@@ -15,7 +17,7 @@ window.addEventListener('DOMContentLoaded', function() {
         var checkurl = /^((https?|http?|file):\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?$/;
         var checksms = /^(smsto?|SMSTO?):([0-9]+):(.+\n.*)$/;
 
-        if (checkurl.test(content)) {
+        if(checkurl.test(content)) {
             $('#PrimaryBtn').text('Redirect');
             $('#PrimaryBtn').click( function() {
                 location.href = newc;
@@ -23,19 +25,19 @@ window.addEventListener('DOMContentLoaded', function() {
             // window.open(content, '_self');
         }
 
-        if (checksms.test(content)) {
+        if(checksms.test(content)) {
             let phonenum = content.replace(/^(smsto?|SMSTO?):([0-9]+):(.*\n.*)$/, '$2');
             let smsbody = content.replace(/^(smsto?|SMSTO?):([0-9]+):(.+\n.*)$/, '$3');
             let newc = 'sms:' + phonenum + '?body=' + encodeURIComponent(smsbody);
             $('#PrimaryBtn').text('Open SMS App');
-            $('#PrimaryBtn').click( function() {
+            $('#PrimaryBtn').click(function() {
                 location.href = newc;
             });
         }
     });
 
     Instascan.Camera.getCameras().then(function (cameras) {
-        if (cameras.length > 1) {
+        if(cameras.length > 1) {
             scanner.start(cameras[1]);
         } else if(cameras.length === 1) {
             scanner.start(cameras[0]);
@@ -43,4 +45,15 @@ window.addEventListener('DOMContentLoaded', function() {
             console.error('😥 No Camera found.');
         }
     }).catch(err => console.error('😥 Something went wrong: ', err));
+
+    window.addEventListener('beforeinstallprompt', function(event) {
+        event.preventDefault();
+        installPromptEvent = event;
+        $('#downloadBtn').click(function() {
+            if(installPromptEvent) {
+                installPromptEvent.prompt();
+                installPromptEvent = null;
+            }
+        });
+    });
 });
--- sw.js
+++ sw.js
@@ -7,6 +7,7 @@ const cacheResources = [
     '/a2hs-qrscanner/images/logo.svg',
     '/a2hs-qrscanner/images/icons/logo-192.png',
     '/a2hs-qrscanner/images/icons/logo-512.png',
+    'https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css',
     'https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css',
     'https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js',
     'https://code.jquery.com/jquery-3.6.0.slim.min.js'
