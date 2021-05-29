94dddb9761801eb62323d6c25e4e8b9557494d15
rti56kt <rti56kt@gmail.com>
2021-05-29 19:25:20 +0800

    [QRSCAN-1] First upload

---
+++ js/main.js
@@ -0,0 +1,46 @@
+if ('serviceWorker' in navigator) {
+    window.addEventListener('DOMContentLoaded', function() {
+        navigator.serviceWorker.register('/a2hs-qrscanner/sw.js')
+        .then(reg => console.log('😎 Service worker registered!', reg))
+        .catch(err => console.error('😥 Service worker registration failed: ', err));
+    });
+}
+
+window.addEventListener('DOMContentLoaded', function() {
+    var scanner = new Instascan.Scanner({ video: document.getElementById('preview'), mirror: false });
+    scanner.addListener('scan', function (content) {
+        $('#exampleModal').modal('show');
+        $('#ModalContent').text(content);
+
+        var checkurl = /^((https?|http?|file):\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?$/;
+        var checksms = /^(smsto?|SMSTO?):([0-9]+):(.+\n.*)$/;
+
+        if (checkurl.test(content)) {
+            $('#PrimaryBtn').text('Redirect');
+            $('#PrimaryBtn').click( function() {
+                location.href = newc;
+            });
+            // window.open(content, '_self');
+        }
+
+        if (checksms.test(content)) {
+            let phonenum = content.replace(/^(smsto?|SMSTO?):([0-9]+):(.*\n.*)$/, '$2');
+            let smsbody = content.replace(/^(smsto?|SMSTO?):([0-9]+):(.+\n.*)$/, '$3');
+            let newc = 'sms:' + phonenum + '?body=' + encodeURIComponent(smsbody);
+            $('#PrimaryBtn').text('Open SMS App');
+            $('#PrimaryBtn').click( function() {
+                location.href = newc;
+            });
+        }
+    });
+
+    Instascan.Camera.getCameras().then(function (cameras) {
+        if (cameras.length > 1) {
+            scanner.start(cameras[1]);
+        } else if(cameras.length === 1) {
+            scanner.start(cameras[0]);
+        } else {
+            console.error('😥 No Camera found.');
+        }
+    }).catch(err => console.error('😥 Something went wrong: ', err));
+});
---
+++ sw.js
@@ -0,0 +1,44 @@
+const cacheName = 'cache-v1';
+const cacheResources = [
+    '/a2hs-qrscanner/',
+    '/a2hs-qrscanner/index.html',
+    '/a2hs-qrscanner/js/main.js',
+    '/a2hs-qrscanner/js/app/instascan.min.js',
+    'https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css',
+    'https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js',
+    'https://code.jquery.com/jquery-3.6.0.slim.min.js'
+]
+
+// install
+self.addEventListener('install', event => {
+    console.log('😏 Service worker is installed!');
+    event.waitUntil(caches.open(cacheName).then((cache) => cache.addAll(cacheResources)));
+});
+
+// activate
+self.addEventListener('activate', event => {
+    console.log('🥳 Service worker is activated!');
+    event.waitUntil(
+        caches.keys().then(function(cacheNameArr) {
+            var newCacheNameArr = cacheNameArr.map(function(item) {
+                if (item !== cacheName) {
+                    return caches.delete(item);
+                }
+            })
+            return Promise.all(newCacheNameArr);
+        })
+    );
+});
+
+// fetch
+self.addEventListener('fetch', event => {
+    console.log('Fetch intercepted for:', event.request.url);
+    event.respondWith(
+        caches.match(event.request).then((cachedResponse) => {
+            if (cachedResponse) {
+                return cachedResponse;
+            }
+            return fetch(event.request);
+        }),
+    );
+});
